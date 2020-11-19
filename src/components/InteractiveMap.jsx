7ac98dbc1153466c77e4abe882d8bbe4e4561e3d
david mosca <david.mosca69@gmail.com>
2020-11-19 23:41:27 +0100

    move markers on paris page

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,52 +1,4 @@
-import { Icon } from "leaflet/src/layer/marker/Icon";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-
-// icons
-import louvre from "../image/louvre.png";
-import ring from "../image/ring.png";
-import elyzeePalace from "../image/Elysee-Palace.png";
-import arche from "../image/arche.png";
-import piece from "../image/piece.png";
-import liasse from "../image/liasse.png";
-
-const louvreIcon = new Icon({
-    iconUrl: louvre,
-    iconSize: [32, 32], // size of the icon
-    iconAnchor: [48.867163198, 2.324332036], // point of the icon which will correspond to marker's location
-    // shadowSize: [50, 64], // size of the shadow
-    // shadowAnchor: [4, 62], // the same for the shadow
-    // popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
-});
-
-const ringIcon = new Icon({
-    iconUrl: ring,
-    iconSize: [64, 64], // size of the icon
-    iconAnchor: [48.864824, 2.334595], // point of the icon which will correspond to marker's location
-});
-
-const elyzeePalaceIcon = new Icon({
-    iconUrl: elyzeePalace,
-    iconSize: [64, 32], // size of the icon
-    iconAnchor: [48.875, 2.31], // point of the icon which will correspond to marker's location
-});
-
-const archeIcon = new Icon({
-    iconUrl: arche,
-    iconSize: [32, 32], // size of the icon
-    iconAnchor: [48.892, 2.24], // point of the icon which will correspond to marker's location
-});
-
-const pieceIcon = new Icon({
-    iconUrl: piece,
-    iconSize: [32, 32], // size of the icon
-    iconAnchor: [48.85, 2.326], // point of the icon which will correspond to marker's location
-});
-
-const liasseIcon = new Icon({
-    iconUrl: liasse,
-    iconSize: [64, 64], // size of the icon
-    iconAnchor: [48.863, 2.276], // point of the icon which will correspond to marker's location
-});
+import { MapContainer, TileLayer } from "react-leaflet";
 
 function InteractiveMap() {
     return (
@@ -61,41 +13,6 @@ function InteractiveMap() {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[48.864824, 2.334595]} icon={louvreIcon}>
-                    <Popup>
-                        Le louvre! <br /> Moulte argent et or
-                    </Popup>
-                </Marker>
-
-                <Marker position={[48.875, 2.33]} icon={ringIcon}>
-                    <Popup>
-                        Place vendôme <br /> Joillerie
-                    </Popup>
-                </Marker>
-
-                <Marker position={[48.875, 2.31]} icon={elyzeePalaceIcon}>
-                    <Popup>
-                        Palais de l'Elysée <br /> Oeuvres d'art
-                    </Popup>
-                </Marker>
-
-                <Marker position={[48.892, 2.24]} icon={archeIcon}>
-                    <Popup>
-                        La grande Arche <br /> Argent
-                    </Popup>
-                </Marker>
-
-                <Marker position={[48.85, 2.326]} icon={pieceIcon}>
-                    <Popup>
-                        Banque de france <br /> Argent
-                    </Popup>
-                </Marker>
-
-                <Marker position={[48.863, 2.276]} icon={liasseIcon}>
-                    <Popup>
-                        16e Arrondissement <br /> Nobles
-                    </Popup>
-                </Marker>
             </MapContainer>
         </div>
     );
