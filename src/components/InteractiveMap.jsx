93eb57d470cfe3312e246d44154a395f4be91231
yoanmarcos <yomarcos@free.fr>
2020-11-19 20:53:12 +0100

    resize the map

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,25 +1,23 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-// import "../../node_modules/leaflet/dist/leaflet";
 
+const position = [51.505, -0.09];
 export default function interactiveMap() {
   return (
-    <div className="App">
-      <MapContainer
-        style={{ height: "400px" }}
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer
+      style={{ height: "400px" }}
+      center={position}
+      zoom={8}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
