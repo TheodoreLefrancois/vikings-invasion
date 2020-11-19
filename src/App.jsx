187457ec0e3a62abba60eb6845b078c8bdad2fb4
RZucconi <romain.zucconi@gmail.com>
2020-11-19 20:34:29 +0100

    filter feature of Paris is now complet

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Filtertools from "./components/FilterTools";
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
       <Filtertools />
     </div>
   );
--- src/api/vikingApi.js
+++ src/api/vikingApi.js
@@ -1,4 +1,4 @@
-import Axios from "axios";
+import axios from "axios";
 import { networks } from "./staticApi";
 
 const PARIS_BASE_URL = "http://restratpws.azurewebsites.net/api/";
@@ -15,19 +15,19 @@ const PARIS_BASE_URL = "http://restratpws.azurewebsites.net/api/";
  * returns networks of Paris city (tram, bus, metro, rer)
  */
 export function getParisNetworks() {
-    return networks;
+  return networks;
 }
 
-export function getParisLines(networtk) {
-    const url = PARIS_BASE_URL.concat(`Lines/${networks}`);
+export function getParisLines(network) {
+  const url = PARIS_BASE_URL.concat(`Lines/${network}`);
 
-    return axiosRequest(url);
+  return axiosRequest(url);
 }
 
 export function getParisLineColor(network, lineId) {
-    const url = PARIS_BASE_URL.concat(`Lines/${networks}/line/${lineId}/color`);
+  const url = PARIS_BASE_URL.concat(`Lines/${network}/line/${lineId}/color`);
 
-    return axiosRequest(url);
+  return axiosRequest(url);
 }
 
 // *******************************************************
@@ -35,9 +35,9 @@ export function getParisLineColor(network, lineId) {
 // *******************************************************
 
 export function getParisStationByLine(lineId) {
-    const url = PARIS_BASE_URL.concat(`Stations/${lineId}`);
+  const url = PARIS_BASE_URL.concat(`Stations/${lineId}`);
 
-    return axiosRequest(url);
+  return axiosRequest(url);
 }
 
 // *******************************************************
@@ -45,9 +45,9 @@ export function getParisStationByLine(lineId) {
 // *******************************************************
 
 export function getParisDirectionsByLine(lineId) {
-    const url = PARIS_BASE_URL.concat(`Directions/${lineId}`);
+  const url = PARIS_BASE_URL.concat(`Directions/${lineId}`);
 
-    return axiosRequest(url);
+  return axiosRequest(url);
 }
 
 // *******************************************************
@@ -55,11 +55,11 @@ export function getParisDirectionsByLine(lineId) {
 // *******************************************************
 
 export function getParisMissionsByStation(lineId, stationId, directionId) {
-    const url = PARIS_BASE_URL.concat(
-        `Missions/${lineId}/from/${stationId}/way/${directionId}`
-    );
+  const url = PARIS_BASE_URL.concat(
+    `Missions/${lineId}/from/${stationId}/way/${directionId}`
+  );
 
-    return axiosRequest(url);
+  return axiosRequest(url);
 }
 
 // *******************************************************
@@ -67,12 +67,13 @@ export function getParisMissionsByStation(lineId, stationId, directionId) {
 // *******************************************************
 
 export function getBordeauxTrams() {
-    const url =
-        "/api/records/1.0/search/?dataset=tb_arret_p&q=&facet=ville&facet=codepost&facet=nature&facet=lignedes&facet=mobilie1&facet=reseau&refine.ville=BORDEAUX&refine.reseau=TRAM&refine.codepost=33000";
+  const url =
+    "/api/records/1.0/search/?dataset=tb_arret_p&q=&facet=ville&facet=codepost&facet=nature&facet=lignedes&facet=mobilie1&facet=reseau&refine.ville=BORDEAUX&refine.reseau=TRAM&refine.codepost=33000";
 
-    return axiosRequest(url);
+  return axiosRequest(url);
 }
 
 function axiosRequest(url) {
-    return Axios.get(url);
+  console.log(url);
+  return axios.get(url);
 }
