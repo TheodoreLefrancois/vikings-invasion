8028f102ff2ad1bb9bd6c225b13584660f92cc51
Malo Pivert <pivertmalo@gmail.com>
2020-11-19 23:18:42 +0100

    footer made with love

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import { BsHeartFill } from "react-icons/bs";
+import { GiWaveSurfer } from "react-icons/gi";
+import { FaMountain } from "react-icons/fa";
+
+import "../style/footer.css";
+
+function Footer() {
+  return (
+    <div className="footerMain">
+      <p>
+        <GiWaveSurfer /> From Biarritz with{" "}
+        <BsHeartFill style={{ color: "red" }} /> by Romain, David, Theodore,
+        Yoan and Malo <FaMountain />
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
