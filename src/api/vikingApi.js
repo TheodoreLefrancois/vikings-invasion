a27e544ee71b52d396ced3f4abfd082a2d076266
david mosca <david.mosca69@gmail.com>
2020-11-20 01:08:11 +0100

    add selected line to map

--- src/api/vikingApi.js
+++ src/api/vikingApi.js
@@ -15,19 +15,19 @@ const PARIS_BASE_URL = "http://restratpws.azurewebsites.net/api/";
  * returns networks of Paris city (tram, bus, metro, rer)
  */
 export function getParisNetworks() {
-  return networks;
+    return networks;
 }
 
 export function getParisLines(network) {
-  const url = PARIS_BASE_URL.concat(`Lines/${network}`);
+    const url = PARIS_BASE_URL.concat(`Lines/${network}`);
 
-  return axiosRequest(url);
+    return axiosRequest(url);
 }
 
 export function getParisLineColor(network, lineId) {
-  const url = PARIS_BASE_URL.concat(`Lines/${network}/line/${lineId}/color`);
+    const url = PARIS_BASE_URL.concat(`Lines/${network}/line/${lineId}/color`);
 
-  return axiosRequest(url);
+    return axiosRequest(url);
 }
 
 // *******************************************************
@@ -35,9 +35,9 @@ export function getParisLineColor(network, lineId) {
 // *******************************************************
 
 export function getParisStationByLine(lineId) {
-  const url = PARIS_BASE_URL.concat(`Stations/${lineId}`);
+    const url = PARIS_BASE_URL.concat(`Stations/${lineId}`);
 
-  return axiosRequest(url);
+    return axiosRequest(url);
 }
 
 // *******************************************************
@@ -45,9 +45,9 @@ export function getParisStationByLine(lineId) {
 // *******************************************************
 
 export function getParisDirectionsByLine(lineId) {
-  const url = PARIS_BASE_URL.concat(`Directions/${lineId}`);
+    const url = PARIS_BASE_URL.concat(`Directions/${lineId}`);
 
-  return axiosRequest(url);
+    return axiosRequest(url);
 }
 
 // *******************************************************
@@ -55,11 +55,11 @@ export function getParisDirectionsByLine(lineId) {
 // *******************************************************
 
 export function getParisMissionsByStation(lineId, stationId, directionId) {
-  const url = PARIS_BASE_URL.concat(
-    `Missions/${lineId}/from/${stationId}/way/${directionId}`
-  );
+    const url = PARIS_BASE_URL.concat(
+        `Missions/${lineId}/from/${stationId}/way/${directionId}`
+    );
 
-  return axiosRequest(url);
+    return axiosRequest(url);
 }
 
 // *******************************************************
@@ -67,13 +67,21 @@ export function getParisMissionsByStation(lineId, stationId, directionId) {
 // *******************************************************
 
 export function getBordeauxTrams() {
-  const url =
-    "/api/records/1.0/search/?dataset=tb_arret_p&q=&facet=ville&facet=codepost&facet=nature&facet=lignedes&facet=mobilie1&facet=reseau&refine.ville=BORDEAUX&refine.reseau=TRAM&refine.codepost=33000";
+    const url =
+        "/api/records/1.0/search/?dataset=tb_arret_p&q=&facet=ville&facet=codepost&facet=nature&facet=lignedes&facet=mobilie1&facet=reseau&refine.ville=BORDEAUX&refine.reseau=TRAM&refine.codepost=33000";
 
-  return axiosRequest(url);
+    return axiosRequest(url);
+}
+
+export function getGeolocalisation(adress) {
+    const url = "https://api-adresse.data.gouv.fr/search/?q="
+        .concat(adress)
+        .concat(" paris");
+
+    return axiosRequest(url);
 }
 
 function axiosRequest(url) {
-  console.log(url);
-  return axios.get(url);
+    console.log(url);
+    return axios.get(url);
 }
--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,21 +1,69 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet/src/layer/marker/Icon";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+// import "../../node_modules/leaflet/dist/leaflet";
+// import Leaflet from "leaflet";
+import viking from "../image/viking_PNG10.png";
+import cible from "../image/cible2.png";
+import glass from "../image/sunglasses_PNG95.png";
 
 function InteractiveMap() {
-    return (
-        <div className="App">
-            <MapContainer
-                style={{ height: "600px", width: "800px" }}
-                center={[48.88, 2.28]}
-                zoom={13}
-                scrollWheelZoom={true}
-            >
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-            </MapContainer>
-        </div>
-    );
-}
+  // const icon = new Icon({
+  //     iconUrl: favicon,
+  // });
 
+  const head = new Icon({
+    iconUrl: viking,
+    iconSize: [60, 50], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [52.520007, 13.404954], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62], // the same for the shadow
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
+
+  const best = new Icon({
+    iconUrl: glass,
+    iconSize: [60, 50], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [52.520007, 13.404954], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62], // the same for the shadow
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
+
+  const victim = new Icon({
+    iconUrl: cible,
+    iconSize: [60, 50], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [44.837789, -0.57918], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62], // the same for the shadow
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
+
+  return (
+    <div className="App">
+      <MapContainer
+        style={{ height: "600px" }}
+        center={[47.3, -1]}
+        zoom={5}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={[52.520007, 13.404954]} icon={head}></Marker>
+        <Marker position={[44.43225, 26.10626]} icon={head}></Marker>
+        <Marker position={[52.370216, 4.895168]} icon={head}></Marker>
+        <Marker position={[41.3833, 2.1833]} icon={head}></Marker>
+        <Marker position={[50.8466, 4.3528]} icon={head}></Marker>
+        <Marker position={[39.1167, -8.6333]} icon={head}></Marker>
+        <Marker position={[51.509093, -0.094151]} icon={head}></Marker>
+        <Marker position={[40.4167754, -3.3037902]} icon={head}></Marker>
+        <Marker position={[45.537789, -0.17918]} icon={victim}></Marker>
+        <Marker position={[49.6534, 3.0488]} icon={victim}></Marker>
+        <Marker position={[43.4831519, -1.558626]} icon={best}></Marker>
+      </MapContainer>
+    </div>
+  );
+}
 export default InteractiveMap;
