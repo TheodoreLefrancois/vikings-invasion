7e5318fbba30763315fcec9e91cacc3c8a7e0b33
Malo Pivert <pivertmalo@gmail.com>
2020-11-20 01:45:16 +0100

    jumbotron added and map and legend repositioned

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,10 +1,12 @@
-
 import { Icon } from "leaflet/src/layer/marker/Icon";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
 // import "../../node_modules/leaflet/dist/leaflet";
 // import Leaflet from "leaflet";
 import viking from "../image/viking_PNG10.png";
 import cible from "../image/cible2.png";
+import MapLegend from "./MapLegend";
+import { Container, Jumbotron } from "reactstrap";
 
 function InteractiveMap() {
   // const icon = new Icon({
@@ -29,31 +31,55 @@ function InteractiveMap() {
     popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
   });
 
-
   return (
-    <div className="App">
-      <MapContainer
-        style={{ height: "400px" }}
-        center={[50, 0]}
-        zoom={4}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+    <>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3" style={{ textAlign: "center" }}>
+            Welcome to RaidAdvisor
+          </h1>
+          <p style={{ color: "black", textAlign: "center" }} className="lead">
+            The perfect place for vikings tourism
+          </p>
+        </Container>
+      </Jumbotron>
 
-        <Marker position={[52.520007, 13.404954]} icon={head}></Marker>
-        <Marker position={[44.43225, 26.10626]} icon={head}></Marker>
-        <Marker position={[52.370216, 4.895168]} icon={head}></Marker>
-        <Marker position={[41.3833, 2.1833]} icon={head}></Marker>
-        <Marker position={[50.8466, 4.3528]} icon={head}></Marker>
-        <Marker position={[39.1167, -8.6333]} icon={head}></Marker>
-        <Marker position={[51.509093, -0.094151]} icon={head}></Marker>
-        <Marker position={[40.4167754, -3.3037902]} icon={head}></Marker>
-        <Marker position={[45.237789, -0.17918]} icon={victim}></Marker>
-      </MapContainer>
-    </div>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <div style={{ width: "50%" }} className="App">
+          <MapContainer
+            style={{ height: "400px" }}
+            center={[47.3, -1]}
+            zoom={4}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[52.520007, 13.404954]} icon={head}></Marker>
+            <Marker position={[44.43225, 26.10626]} icon={head}></Marker>
+            <Marker position={[52.370216, 4.895168]} icon={head}></Marker>
+            <Marker position={[41.3833, 2.1833]} icon={head}></Marker>
+            <Marker position={[50.8466, 4.3528]} icon={head}></Marker>
+            <Marker position={[39.1167, -8.6333]} icon={head}></Marker>
+            <Marker position={[51.509093, -0.094151]} icon={head}></Marker>
+            <Marker position={[40.4167754, -3.3037902]} icon={head}></Marker>
+            <Marker position={[45.237789, -0.17918]} icon={victim}></Marker>
+          </MapContainer>
+        </div>
+        <div
+          style={{
+            width: "25%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <MapLegend />
+        </div>
+      </div>
+    </>
   );
 }
 export default InteractiveMap;
--- src/components/MapLegend.jsx
+++ src/components/MapLegend.jsx
@@ -6,49 +6,45 @@ import { BsHeartFill } from "react-icons/bs";
 
 function MapLegend() {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div style={{ width: "25%", paddingTop: "1em" }}>
-        <Table dark borderless>
-          <thead>
-            <tr>
-              <th>Legend</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row" style={{ width: "90px" }}>
-                <img
-                  src={viking}
-                  alt="viking head with haxes"
-                  style={{ width: "100%" }}
-                />
-              </th>
+    <Table dark borderless>
+      <thead>
+        <tr>
+          <th>Legend</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row" style={{ width: "90px" }}>
+            <img
+              src={viking}
+              alt="viking head with haxes"
+              style={{ width: "100%" }}
+            />
+          </th>
 
-              <td>Villages Destroyed</td>
-              <td>8</td>
-            </tr>
-            <tr>
-              <th scope="row" style={{ width: "90px" }}>
-                <img
-                  src={cible}
-                  alt="viking head with haxes"
-                  style={{ width: "100%" }}
-                />
-              </th>
-              <td>Next Targets</td>
-              <td>2</td>
-            </tr>
-            <tr>
-              <th scope="row">NEXT ICON HERE</th>
-              <td>Home sweet home</td>
-              <td>
-                <BsHeartFill style={{ color: "red" }} />
-              </td>
-            </tr>
-          </tbody>
-        </Table>
-      </div>
-    </div>
+          <td>Villages Destroyed</td>
+          <td>8</td>
+        </tr>
+        <tr>
+          <th scope="row" style={{ width: "90px" }}>
+            <img
+              src={cible}
+              alt="viking head with haxes"
+              style={{ width: "100%" }}
+            />
+          </th>
+          <td>Next Targets</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <th scope="row">NEXT ICON HERE</th>
+          <td>Home sweet home</td>
+          <td>
+            <BsHeartFill style={{ color: "red" }} />
+          </td>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
 
